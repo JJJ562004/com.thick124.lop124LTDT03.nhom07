e568c37ba1c62adf5d3875c1e5977c762536e56e
JJJ562004 <darknessrisesgamer999@gmail.com>
2024-12-21 09:51:13 +0700

    feature/member_infos

--- app/src/main/java/com/thicuoiky124/lttd03/nhom07/MainActivity.java
+++ app/src/main/java/com/thicuoiky124/lttd03/nhom07/MainActivity.java
@@ -10,11 +10,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public CustomViewPager customViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        customViewPager = findViewById(R.id.main_viewpager);
+        ViewPageAdapter adapter = new ViewPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new thongtincanhan()); //0
+
+        customViewPager.setAdapter(adapter);
+        customViewPager.setSwipeEnabled(false);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
